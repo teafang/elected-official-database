512cec1beb321e3a226c72d0c83ad13e8608e82f
Charlotte <charlottey820@gmail.com>
2020-07-23 15:49:46 -0400

    hardcoded list of address->senators, list of senators->info. added for loop in results.html to loop through all elected officials for each address

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ from flask import request
 # -- Initialization section --
 app = Flask(__name__)
 
+address_to_officials = {
+    '123 Chambers St':['Chuck Schumer','Kirsten Gillibrand'],
+    '456 Main St':['Kirsten Gillibrand','Chuck Schumer'],
+}
+
+senator_info = {
+    'Chuck Schumer':{'name':'Chuck Schumer','office':'Senator'},
+    'Kirsten Gillibrand':{'name':'Kirsten Gillibrand','office':'Senator'},
+}
 
 # -- Routes section --
 @app.route('/')
@@ -22,7 +31,9 @@ def results():
     else:
         form = request.form
         address = form["address"]
+        senator = address_to_officials[address]
         data={
-            'address':address,
+            'senator':senator,
+            'senator_info':senator_info,
         }
     return render_template("results.html", data=data)
