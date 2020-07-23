b461fc7a58a6449256a6797dd76743914ce87aea
Taylor Fang <taylor.fang15@gmail.com>
2020-07-23 10:10:08 -0600

    testing

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
 
 
 # -- Initialization section --
